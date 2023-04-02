5224601f61d7e7244066f7c30ca841db1379c46a
Shivam-Attri <shivam.attri2020@gmail.com>
2023-04-02 22:16:35 +0530

    Inital Commit

---
+++ src/test/java/com/shivam/Employee/Directory/EmployeeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.Employee.Directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
